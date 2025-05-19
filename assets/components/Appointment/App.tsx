b558c84ddcb1b080c23474fd60d81de81df72f52
Kadem <kadem.garnier@gmail.com>
2025-05-19 10:53:55 +0200

    add react component

---
+++ assets/components/Appointment/App.tsx
@@ -0,0 +1,8 @@
+export function App(){
+    return (
+        <div>
+            <h1>My App</h1>
+            <p>This is a simple app.</p>
+        </div>
+    );
+}
--- assets/front.ts
+++ assets/front.ts
@@ -1,1 +1,5 @@
 import './style/front/front.scss'
+import registerPreactCustomElement from "./function/registerPreactCustomElement";
+import {App} from "./components/Appointment/App";
+
+registerPreactCustomElement(App, 'make-appointment', '', '')
