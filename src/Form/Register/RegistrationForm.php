02c854ccc6896a1534c49e23497b6e9eff15ed7a
kasraoui acil <acilk@MacBook-Pro-2.local>
2025-05-22 16:55:50 +0200

    correction erreur

---
+++ src/Form/Register/RegistrationForm.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Form\Register;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('civility', ChoiceType::class, [
+                'label' => 'Vous êtes',
+                'choices' => [
+                    'Monsieur' => 'Monsieur',
+                    'Madame' => 'Madame',
+                ],
+            ])
+            ->add('type', ChoiceType::class, [
+                'label' => 'Je suis',
+                'choices' => [
+                    'Un particulier' => 'particulier',
+                    'Un professionnel' => 'professionnel',
+                ],
+                'expanded' => true,
+                'multiple' => false,
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre adresse mail',
+                'required' => true,
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'required' => true,
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'required' => true,
+            ])
+            ->add('tel', TextType::class, [
+                'label' => 'Votre numéro de téléphone',
+                'required' => true,
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'label' => 'Votre mot de passe',
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'label' => 'J\'accepte les conditions générales de ventes',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
