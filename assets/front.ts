f955aed101649319946467afb891a35a781bf041
Kadem <kadem.garnier@gmail.com>
2025-05-22 14:57:41 +0200

    merge and rebase

--- assets/front.ts
+++ assets/front.ts
@@ -2,9 +2,10 @@ import './style/front/front.scss'
 import registerPreactCustomElement from "./function/registerPreactCustomElement";
 import {App} from "./components/Appointment/App";
 import register from "./function/register";
+import {AlertMessage} from "./elements/AlertMessage.ts";
 
 registerPreactCustomElement(App, 'make-appointment', '', '')
-
+customElements.define('alert-message', AlertMessage)
 
 document.addEventListener("DOMContentLoaded", () => {
     register();
